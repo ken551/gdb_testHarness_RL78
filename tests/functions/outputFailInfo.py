da4f4799feaf9567cbb3c0574068892a92f2b24e
ken551 <hide.delorean@gmail.com>
2023-11-21 20:18:27 +0900

    e2studio上でハイライトされるよう出力文を修正

--- tests/functions/outputFailInfo.py
+++ tests/functions/outputFailInfo.py
@@ -2,5 +2,5 @@ testName = getValFromGdb("testName")
 testName = testName.replace("\\000",'')
 testName = testName.replace("\n",'')
 assertNum = getIntFromGdb("assertNum")
-print "in test " + testName
-print "!!!!!!!!test assert no."+str(assertNum)+" failed!!!!!!"
+print "E000000: in test " + testName
+print "E000000: test assert no."+str(assertNum)+" failed!"
--- tests/functions/testEnd.py
+++ tests/functions/testEnd.py
@@ -3,6 +3,6 @@ if assertResult != 0:
     gdb.execute("set $fails = ($fails + 1)")
     testName = getValFromGdb("testName")
     testName = testName.replace("\n","")
-    print "testcase " + testName + " failed.\n"
+    print "E000000: testcase " + testName + " failed\n"
 else:
     gdb.execute("set $oks = ($oks + 1)")
